ffa65218bb09abcfe18e1cb5a71fe9186903322f
Karim <karim2904@gmail.com>
2015-03-12 20:20:38 +0100

    true/false matrix

--- app.js
+++ app.js
@@ -1,4 +1,38 @@
 var fs = require("fs");
 
-var input = fs.readFileSync('test.in', 'utf-8');
-console.log(input);
+var input = fs.readFileSync('test.in', 'utf-8').split('\n');
+
+var inputConfig = input[0].split(' ');
+
+var R = inputConfig[0];
+var S = inputConfig[1];
+var U = inputConfig[2];
+var P = inputConfig[3];
+var M = inputConfig[4];
+
+
+// Creating true/false matrix
+var matriceDispo = new Array();
+for (var i = 0; i < R; i++)
+{
+	var row = new Array();
+	for (var j = 0; j < S; j++)
+	{
+		row[j] = true;
+	}
+	matriceDispo[i] = row;
+	
+}
+
+for (var i = 0; i < U; i++)
+{
+	var uSlot = input[i+1].split(' ');
+	var x = uSlot[0];
+	var y = uSlot[1];
+	
+	//console.log(x+ ' : '+y);
+	
+	matriceDispo[uSlot[0]][uSlot[1]] = false;
+}
+
+console.log(matriceDispo);
