baca65c8da5a2e08fe5dbbeb59bfec19dd553fde
mkitane <mehdikitane@gmail.com>
2015-03-12 22:10:07 +0100

    app.js

--- app.js
+++ app.js
@@ -50,11 +50,105 @@ for (var i = 0; i < M; i++)
 		x: 0,
 		y: 0,
 		group: 0,
-		score: 0
+		score: 0, 
+		place : false
 	});
 	//console.log("Server "+ serverInfo);
 }
 
-var newTableServers = naive.arrange_groups_native(servers, matriceDispo);
-naive.output(newTableServers);
-//console.log(servers);
+
+
+
+
+
+
+servers = sortServers(servers);
+console.log(servers);
+
+
+var mon_result = fillList(servers, matriceDispo); 
+servers = mon_result.servers; 
+matriceDispo = mon_result.matriceDispo; 
+
+console.log(servers);
+
+function sortServers(listServers)
+{
+	var facteurTaille = 0.8; 
+	var facteurPoids = 0.2; 
+
+
+	for(var i= 0; i<M; i++)
+	{
+		var serverInfo = listServers[i]; 
+		serverInfo.score = facteurTaille * serverInfo.slots + facteurPoids*serverInfo.cap;
+	}
+
+	listServers = listServers.sort(function(a, b) {
+    	return b.score - a.score;
+	});
+	return listServers;
+}
+
+function fillList(listServers, listeDoccupation)
+{
+	var dernierServeurPlace = -1; 
+	for(var i = 0 ; i<listServers.length; i++)
+	{
+		var monServeurAplacer = listServers[i];
+		//On boucle sur tableau
+		for(var a=0 ; a<listeDoccupation.length; a++)
+		{
+			//var j = a;
+			var estPlace = false; 
+			var j = (a + dernierServeurPlace + 1) % listeDoccupation.length ;
+
+			for(var k=0; k<listeDoccupation[j].length ; k++)
+			{
+
+				var estPlacable = true;
+                var testA = listeDoccupation[j][k] == true;
+                var testB = (  (k + monServeurAplacer.slots) <= listeDoccupation[j].length );
+				if(  testA &&  testB  )
+				{
+					for(var h = k ; h < k + monServeurAplacer.slots ; h++)
+					{
+						if( listeDoccupation[j][h] == false)
+						{
+							estPlacable = false; 
+							break;
+						}
+					}
+
+					if(estPlacable)
+					{
+						console.log("on arrive");
+						//On remplit : 
+						for(var h = k; h < k + monServeurAplacer.slots ; h++ )
+						{
+							listeDoccupation[j][h] = false; 
+						}
+						monServeurAplacer.x = j ; 
+						monServeurAplacer.y = k ; 
+						monServeurAplacer.place = true; 
+						dernierServeurPlace = j ;
+
+						estPlace = true; 
+						break; 
+					}else
+					{
+						console.log("on arrive pas a le placer"); 
+					}
+				}
+			}
+			if(estPlace){
+				break;
+			} 
+		}
+	}
+
+	return {
+		servers : listServers, 
+		matriceDispo : listeDoccupation
+	};
+}
